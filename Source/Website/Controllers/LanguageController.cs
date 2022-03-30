b0c9b8a535c42bbe37411a452794af8531739c6a
Phap Dieu Duong <ddphap@gmail.com>
2022-03-31 00:24:04 +0800

    load language list from crowndin

--- Source/Website/Controllers/LanguageController.cs
+++ Source/Website/Controllers/LanguageController.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImageGlass.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ImageGlass.Controllers;
 
 public class LanguageController : Controller
 {
-    public IActionResult Index()
+    const string CROWNDIN_KEY = "0b08634573c456476345efa8bad174f2";
+
+    private static JsonSerializerOptions JsonOptions { get; } = new()
     {
-        return View();
+        PropertyNameCaseInsensitive = true,
+        AllowTrailingCommas = true,
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+
+        Converters =
+        {
+            // Write enum value as string
+            new JsonStringEnumConverter(),
+        },
+
+        // ignoring policy
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault | JsonIgnoreCondition.WhenWritingNull,
+        IgnoreReadOnlyProperties = true,
+        IgnoreReadOnlyFields = true,
+    };
+
+    public async Task<IActionResult> Index()
+    {
+        var url = $"https://api.crowdin.com/api/project/imageglass/status?key={CROWNDIN_KEY}&json";
+
+        var requestMsg = new HttpRequestMessage(HttpMethod.Get, url);
+        using var httpClient = new HttpClient();
+        var response = await httpClient.SendAsync(requestMsg);
+        var languageList = new List<LanguageModel>();
+
+        if (response.IsSuccessStatusCode)
+        {
+            using var contentStream = await response.Content.ReadAsStreamAsync();
+
+            languageList = await JsonSerializer.DeserializeAsync
+                <List<LanguageModel>>(contentStream, JsonOptions);
+        }
+
+        return View(languageList);
     }
 }
---
+++ Source/Website/Models/LanguageModel.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+namespace ImageGlass.Models;
+
+public class LanguageModel
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public string Phrases { get; set; }
+    public string Translated { get; set; }
+    public string Approved { get; set; }
+    public string Words { get; set; }
+    public string Words_translated { get; set; }
+    public string Words_approved { get; set; }
+    public int Translated_progress { get; set; }
+    public int Approved_progress { get; set; }
+    public int Qa_issues { get; set; }
+}
