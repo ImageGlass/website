16ce95ba6184fdea4f56c50953e248c24d8db78d
Phap Dieu Duong <ddphap@gmail.com>
2022-11-06 16:38:58 +0800

    BaseController: use PageInfo in ViewData

--- Source/Website/Controllers/BaseController.cs
+++ Source/Website/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImageGlass.Utils;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ImageGlass.Controllers;
@@ -20,18 +21,19 @@ public class BaseController : Controller
         var con = (Controller)context.Controller;
 
         // set basic meta data
-        con.ViewData["_Name"] = "ImageGlass";
-        con.ViewData["_Sitename"] = "ImageGlass - A lightweight, versatile image viewer";
+        con.ViewData[PageInfo.Name] = "ImageGlass";
+        con.ViewData[PageInfo.SiteName] = "ImageGlass - A lightweight, versatile image viewer";
         
-		con.ViewData["_Title"] = "ImageGlass - A lightweight, versatile image viewer";
-		con.ViewData["_Description"] = "ImageGlass is a lightweight, open source photo viewer that designed to take place Windows Photo Viewer, work with all image formats, includes GIF, SVG, HEIC.";
-        con.ViewData["_Keywords"] = "free photo viewer, best photo viewer, simple fast image viewer, windows 10 photo viewer, lightweight image viewer, best photo viewer for windows 10, open source image viewer, fast switch between images, photo viewer, minimalist image viewer, quick photo viewer, image converter, extract gif file, alternative windows 10, Irfanview alternatives, animated GIF files, imageglass, duong dieu phap, raw photo viewer, heic viewer, svg viewer, color picker, rapid viewing of pictures";
+		con.ViewData[PageInfo.Title] = "ImageGlass - A lightweight, versatile image viewer";
+		con.ViewData[PageInfo.Description] = "ImageGlass is a lightweight, open source photo viewer that designed to take place Windows Photo Viewer, work with all image formats, includes GIF, SVG, HEIC.";
+        con.ViewData[PageInfo.Keywords] = "free photo viewer, best photo viewer, simple fast image viewer, windows 10 photo viewer, lightweight image viewer, best photo viewer for windows 10, open source image viewer, fast switch between images, photo viewer, minimalist image viewer, quick photo viewer, image converter, extract gif file, alternative windows 10, Irfanview alternatives, animated GIF files, imageglass, duong dieu phap, raw photo viewer, heic viewer, svg viewer, color picker, rapid viewing of pictures";
         
-        con.ViewData["_H1"] = "ImageGlass is a lightweight, versatile image viewing application that is designed to take the place of Photo Viewer in Windows 10, 8, 7 and Vista, especially those installations that may have trouble displaying PNG and GIF files in Photo Viewer.";
+        con.ViewData[PageInfo.H1] = "ImageGlass is a lightweight, versatile image viewing application that is designed to take the place of Photo Viewer in Windows 10, 8, 7 and Vista, especially those installations that may have trouble displaying PNG and GIF files in Photo Viewer.";
         
-		con.ViewData["_Author"] = "Duong Dieu Phap";
-        con.ViewData["_Thumbnail"] = "";
+		con.ViewData[PageInfo.Author] = "Duong Dieu Phap";
+        con.ViewData[PageInfo.Thumbnail] = "";
     }
+
 
     /// <summary>
     /// Determines whether the specified HTTP request is an AJAX request.
