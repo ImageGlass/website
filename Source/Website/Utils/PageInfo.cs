2f42216459f752a51ee097c695d21382b89819e6
Phap Dieu Duong <ddphap@gmail.com>
2022-11-06 16:38:11 +0800

    add PageInfo

---
+++ Source/Website/Utils/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ImageGlass.Utils;
+
+public static class PageInfo
+{
+    public static string Name = "_Name";
+    public static string SiteName = "_Sitename";
+
+    public static string Title = "_Title";
+    public static string Description = "_Description";
+    public static string Keywords = "_Keywords";
+    public static string H1 = "_H1";
+
+    public static string Author = "_Author";
+    public static string Thumbnail = "_Thumbnail";
+}
