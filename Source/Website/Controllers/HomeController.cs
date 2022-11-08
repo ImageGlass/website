6643de0918a04b755dc03ca635e01ab03c1b6bf5
Phap Dieu Duong <ddphap@gmail.com>
2022-11-08 12:43:20 +0800

    updated HomeController

--- Source/Website/Controllers/HomeController.cs
+++ Source/Website/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using ImageGlass.Models;
+﻿using ImageGlass.Data;
+using ImageGlass.Models;
+using ImageGlass.Utils;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,22 +8,33 @@ namespace ImageGlass.Controllers;
 
 public class HomeController : BaseController
 {
+    private readonly ImageGlassContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ImageGlassContext context, ILogger<HomeController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
+
     public IActionResult Index()
     {
-        return View();
+        return View("HomePage");
     }
 
-    public IActionResult Privacy()
+
+    [HttpGet("privacy")]
+    public IActionResult PrivacyPage()
     {
-        return View();
+        // page info
+        ViewData[PageInfo.Title] = $"Privacy | {ViewData[PageInfo.Name]}";
+        ViewData[PageInfo.Description] = "ImageGlass privacy information";
+        ViewData[PageInfo.Keywords] = $"imageglass privacy, {ViewData[PageInfo.Keywords]}";
+        
+        return View("PrivacyPage");
     }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
