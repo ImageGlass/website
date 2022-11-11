6d7b6edd4177060920861f019fe1c26e811809d8
Phap Dieu Duong <ddphap@gmail.com>
2022-11-11 23:46:39 +0800

    updated sql

--- Source/Website/Models/NewsModel.cs
+++ Source/Website/Models/NewsModel.cs
@@ -10,17 +10,24 @@ public class NewsModel : BaseModel
     [Required]
     [Column(TypeName = "text")]
     public string Slug { get; set; }
+
+
     [Column(TypeName = "text")]
     public string Title { get; set; } = string.Empty;
+
+
     [Column(TypeName = "text")]
     public string Image { get; set; } = string.Empty;
+
+
     [Column(TypeName = "text")]
     public string Description { get; set; } = string.Empty;
 
+
     /// <summary>
     /// By default, the content will be fetched from this folder:
-    /// https://github.com/ImageGlass/website-content/tree/main/news/.
-    /// This field allows to use a custom url markdown content.
+    /// https://github.com/ImageGlass/website-content/tree/main/news,
+    /// this field allows to use a custom url markdown content.
     /// </summary>
     public string CustomContentUrl { get; set; } = string.Empty;
 
--- Source/Website/Models/ThemeModel.cs
+++ Source/Website/Models/ThemeModel.cs
@@ -9,26 +9,49 @@ public class ThemeModel : BaseModel
 {
     [Column(TypeName = "text")]
     public string Slug { get; set; } = string.Empty;
+
+
     [Column(TypeName = "text")]
     public string Title { get; set; } = string.Empty;
+
+
     [Column(TypeName = "text")]
     public string Image { get; set; } = string.Empty;
+    
+    
     [Column(TypeName = "text")]
     public string Description { get; set; } = string.Empty;
+
+
     public bool IsDarkMode { get; set; } = true;
+    
+    
     [Column(TypeName = "text")]
     public string Link { get; set; } = string.Empty;
+    
+    
     [Column(TypeName = "text")]
     public string Version { get; set; } = string.Empty;
+    
+    
     [Column(TypeName = "text")]
     public string Compatibility { get; set; } = string.Empty;
+    
+    
     [Column(TypeName = "text")]
     public string Author { get; set; } = string.Empty;
+    
+    
     [Column(TypeName = "text")]
     public string Email { get; set; } = string.Empty;
+    
+    
     [Column(TypeName = "text")]
     public string Website { get; set; } = string.Empty;
+
+    
     public int Count { get; set; } = 0;
+
 
     /// <summary>
     /// Gets the string combined with <see cref="Slug"/> and <see cref="Id"/>.
