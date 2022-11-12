232c13e71e658e5a2565dc62f49a5af1f175a460
Phap Dieu Duong <ddphap@gmail.com>
2022-11-12 16:51:39 +0800

    added sql for releases

---
+++ Source/Website/SQL/Requirements.sql
@@ -0,0 +1,18 @@
+
+
+
+INSERT INTO [Requirements]
+([Id], [Content], [CreatedDate], [UpdatedDate], [IsVisible]) VALUES
+
+(1, '<li>.NET Framework 2.0 Runtime</li><li>Windows XP, Vista, 7, 8</li>', '2010-07-22 00:00:00', '2010-07-22 00:00:00', 1),
+
+(2, '<li>.NET Framework 3.5 Runtime</li><li>Windows XP, Vista, 7, 8</li>', '2011-12-22 00:00:00', '2011-12-22 00:00:00', 1),
+
+(3, '<li>.NET Framework 4.0 Runtime</li><li>Windows XP, Vista, 7, 8, 8.1, 10</li>', '2012-11-11 00:00:00', '2012-11-11 00:00:00', 1),
+
+(4, '<li><a href="https://dotnet.microsoft.com/en-us/download/dotnet-framework/net471" target="_blank" rel="noopener nofollow">.NET Framework 4.7.1 Runtime</a></li><li>Windows XP, Vista, 7, 8, 8.1, 10</li>', '2018-05-06 21:11:07', '2018-05-06 21:11:07', 1),
+
+(5, '<li><a href="https://dotnet.microsoft.com/en-us/download/dotnet-framework/net48" target="_blank" rel="noopener nofollow">.NET Framework 4.8 Runtime</a></li><li>Windows XP, Vista, 7, 8, 8.1, 10, 11</li>', '2020-12-08 10:09:40', '2020-12-08 10:09:40', 1),
+
+(6, '<li><a href="https://dotnet.microsoft.com/en-us/download/dotnet/6.0" target="_blank" rel="noopener nofollow">.NET Desktop Runtime 6.0</a></li><li>Windows 7 SP1 ESU, 8, 8.1, 10, 11</li>', '2022-05-14 16:45:44', '2022-05-14 16:45:44', 1);
+
