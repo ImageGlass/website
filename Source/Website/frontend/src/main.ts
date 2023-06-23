4a3cf0ef1394b6ee47227f3f03f92459b4f04ed2
Phap Dieu Duong <ddphap@gmail.com>
2023-06-23 11:38:25 +0700

    header: apply fluent effect

--- Source/Website/frontend/src/main.ts
+++ Source/Website/frontend/src/main.ts
@@ -1,2 +1,10 @@
 ﻿import './styles/main.scss';
 
+import { applyEffect } from 'fluent-reveal-effect';
+
+// fluent effect for header bar
+applyEffect('.navbar-nav .nav-item > a', {
+  clickEffect: true,
+  lightColor: 'rgb(var(--colorInvert) / 0.15)',
+  gradientSize: 100,
+});
