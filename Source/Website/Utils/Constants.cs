0bcef535455432b16aee892045f579f2426a367a
Phap Dieu Duong <ddphap@gmail.com>
2023-06-24 23:45:34 +0800

    add DateTimeExtensions

--- Source/Website/Utils/Constants.cs
+++ Source/Website/Utils/Constants.cs
@@ -3,7 +3,7 @@ namespace ImageGlass.Utils;
 
 public class Constants
 {
-    public const string DATETIME_FORMAT = "yyyy/MM/dd HH:mm:ss";
-    public const string DATE_FORMAT = "yyyy/MM/dd";
+    public const string DATETIME_FORMAT = "MMM dd, yyyy HH:mm tt";
+    public const string DATE_FORMAT = "MMM dd, yyyy";
 }
 
---
+++ Source/Website/Utils/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ImageGlass.Utils;
+
+public static class DateTimeExtensions
+{
+    /// <summary>
+    /// Formats the <see cref="DateTime"/> to string using <see cref="Constants.DATETIME_FORMAT"/>.
+    /// </summary>
+    public static string ToDateTimeString(this DateTime dt)
+    {
+        return dt.ToString(Constants.DATETIME_FORMAT);
+    }
+
+
+    /// <summary>
+    /// Formats the <see cref="DateTime"/> to string using <see cref="Constants.DATE_FORMAT"/>.
+    /// </summary>
+    public static string ToDateString(this DateTime dt)
+    {
+        return dt.ToString(Constants.DATETIME_FORMAT);
+    }
+}
+
