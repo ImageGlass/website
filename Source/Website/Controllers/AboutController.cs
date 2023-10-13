a6faa788d47a1df03e183290f5c2ed633b50e169
Phap Dieu Duong <ddphap@gmail.com>
2023-10-13 12:31:39 +0800

    about page

--- Source/Website/Controllers/AboutController.cs
+++ Source/Website/Controllers/AboutController.cs
@@ -1,13 +1,29 @@
-﻿using ImageGlass.Utils;
+﻿using ImageGlass.Data;
+using ImageGlass.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ImageGlass.Controllers;
 
 public class AboutController : BaseController
 {
-    public IActionResult Index()
+    private readonly ImageGlassContext _context;
+
+
+    public AboutController(ImageGlassContext context)
     {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index(int? page)
+    {
+        // page info
         ViewData[PageInfo.Page] = "about";
+        ViewData[PageInfo.Title] = $"About | {ViewData[PageInfo.Name]}";
+        ViewData[PageInfo.Description] = "ImageGlass' author story";
+        ViewData[PageInfo.Keywords] = $"imageglass author, duong dieu phap, {ViewData[PageInfo.Keywords]}";
+
+        // get news items
+        ViewData[PageInfo.SidebarList] = await _context.QueryNewsModels(5);
 
         return View("AboutPage");
     }
