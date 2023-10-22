7c53b0e86ed5892387a83cd3e816b3caa8e4fbac
Phap Dieu Duong <ddphap@gmail.com>
2023-10-22 23:09:02 +0800

    add tools page

---
+++ Source/Website/Controllers/ToolController.cs
@@ -0,0 +1,32 @@
+﻿using ImageGlassWeb.Data;
+using ImageGlassWeb.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGlassWeb.Controllers;
+
+public class ToolController : BaseController
+{
+    private readonly ImageGlassContext _context;
+
+
+    public ToolController(ImageGlassContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet("tools")]
+    public async Task<IActionResult> ToolListing()
+    {
+        // page info
+        ViewData[PageInfo.Page] = "download.tool";
+        ViewData[PageInfo.Title] = $"Tools | {ViewData[PageInfo.Name]}";
+        ViewData[PageInfo.Description] = "A collection of external tools that can be downloaded and installed to enhance your experience with ImageGlass 9.";
+        ViewData[PageInfo.Keywords] = $"imageglass tools, exif tool, exifglass, {ViewData[PageInfo.Keywords]}";
+
+
+        ViewData[PageInfo.SidebarList] = await _context.QueryNewsModels(5);
+
+        return View("ToolListingPage");
+    }
+}
