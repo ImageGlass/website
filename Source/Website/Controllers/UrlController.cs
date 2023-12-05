4d47b9c69ab89974b1af38020683ab61095467cc
Phap Dieu Duong <ddphap@gmail.com>
2023-12-05 18:29:52 +0800

    add legacy url: /checkforupdate

--- Source/Website/Controllers/UrlController.cs
+++ Source/Website/Controllers/UrlController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImageGlassWeb.Data;
+using ImageGlassWeb.Models;
+using ImageGlassWeb.Utils;
+using Microsoft.AspNetCore.Mvc;
 using System.Text;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -7,6 +10,14 @@ namespace ImageGlassWeb.Controllers;
 
 public class UrlController : BaseController
 {
+    private readonly ImageGlassContext _context;
+
+    public UrlController(ImageGlassContext context)
+    {
+        _context = context;
+    }
+
+
     /// <summary>
     /// Gets update information.
     /// </summary>
@@ -19,6 +30,31 @@ public class UrlController : BaseController
         var jsonStr = await httpClient.GetStringAsync(updateInfoJsonUrl);
 
         return Content(jsonStr, "application/json", Encoding.UTF8);
+    }
+
+
+    /// <summary>
+    /// Check for update (legacy versions: v8.5 or below)
+    /// </summary>
+    [HttpGet("checkforupdate")]
+    public async Task<ActionResult> GetUpdateLegacyAsync()
+    {
+        // featured content
+        var releaseList = await _context.QueryReleaseModels(1, releaseChannel: ReleaseChannel.Stable);
+        var latestStableRelease = releaseList.FirstOrDefault();
+
+
+        var xml = $"""
+<ImageGlass>
+    <Update>
+        <Info newVersion="{latestStableRelease?.Version}" versionType="stable" level="Recommended"
+            link="https://imageglass.org/news/{latestStableRelease?.News?.SlugAndId}" size="30.7 MB"
+            pubDate="{latestStableRelease?.UpdatedDate.ToDateTimeString()}"
+            decription="https://imageglass.org/release/{latestStableRelease?.SlugAndId}"/>
+    </Update>
+</ImageGlass>
+""";
+        return Content(xml, "text/xml", Encoding.UTF8);
     }
 }
 
